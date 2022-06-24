c4c8dc40b162bf78e65c07b1c5f9ce39c7c2aa2f
Aniket Das <aniketdas001@gmail.com>
2022-06-24 16:55:04 -0700

    desktop navbar works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from "react";
-import whiteMenu from "../imgs/black-menu.png"
+import whiteMenu from "../imgs/black-menu.png";
+
+const delay = 5;
 
 export default function Navbar() {
 	const [opacity, setOpacity] = useState(100);
 	const [hidden, setHidden] = useState("invisible");
-	const [bgButton, setBgButton] = useState("transparent")
+	const [hover, setHover] = useState(false);
+	const [bgButton, setBgButton] = useState("transparent");
 
 	useEffect(() => {
-		setTimeout(() => {
+		console.log("called")
+		let timer = setTimeout(() => {
+			console.log(0);
 			setOpacity(0);
-		}, 500);
-	}, []);
+			console.log('opacity is',opacity)
+		}, delay * 100);
+		return () => {
+			clearTimeout(timer);
+			console.log('return', opacity)
+		};
+	}, [opacity]);
 
 	function scrollTo(id) {
 		const anchor = document.querySelector(id);
@@ -18,18 +28,17 @@ export default function Navbar() {
 	}
 
 	function toggleOpacity() {
-		console.log('called')
-		if (hidden==="invisible") {
+		console.log("called");
+		if (hidden === "invisible") {
 			setOpacity(100);
-			setBgButton("black")
+			setBgButton("black");
 			setTimeout(() => {
-				setHidden("visible")
+				setHidden("visible");
 			}, 500);
 		} else {
-			setBgButton("transparent")
-
+			setBgButton("transparent");
 			setTimeout(() => {
-				setHidden("invisible")
+				setHidden("invisible");
 			}, 500);
 		}
 	}
@@ -41,10 +50,13 @@ export default function Navbar() {
 				onClick={() => {
 					toggleOpacity();
 				}}>
-				<img src={whiteMenu} alt="yoyo"/>
+				<img src={whiteMenu} alt="menu" />
 			</button>
 			<div
-				className={`fixed mt-9 ${hidden} sm:visible sm:mt-0 w-screen z-50 opacity-${opacity} hover:opacity-100 transition ease-linear duration-500`}>
+				style = {opacity===100 || hover? {opacity:100} :{opacity: 0}}
+				onMouseOver={() => setHover(true)}
+				onMouseOut={() => setHover(false)}
+				className={`fixed mt-9 ${hidden} sm:visible sm:mt-0 w-screen z-50 hover:opacity-100 transition ease-linear duration-500`}>
 				<ul className="sm:flex bg-black items-center justify-center text-center sm:h-20 z-50">
 					<li className="sm:mr-32">
 						<button
