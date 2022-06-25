da2cc5bd72157a0a84a35974ff0460f231b6ae82
Aniket Das <aniketdas001@gmail.com>
2022-06-25 13:22:48 -0700

    added link for cv

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 export default function About() {
+	const aStyle = "font-light hover:underline";
 	return (
 		<div
 			className="w-screen h-screen overflow-hidden bg-[url('./imgs/aboutbg.jpg')] bg-center bg-cover bg-fixed"
 			id="about">
 			<div
-				className="w-11/12 sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 2xl:w-1/2 rounded bg-white/75 ml-3 sm:ml-4 md:ml-6 lg:ml-8 xl:ml-12 mt-3 sm:mt-4 md:mt-5 lg:mt-7 xl:mt-10 shadow-lg p-5 sm:p-6 lg:p-7 xl:p-8 2xl:p-12"
-				data-aos="zoom-in" data-aos-duration="700">
+				className="w-11/12 sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 2xl:w-1/2 rounded bg-white/80 ml-3 sm:ml-4 md:ml-6 lg:ml-8 xl:ml-12 mt-3 sm:mt-4 md:mt-5 lg:mt-7 xl:mt-10 shadow-lg p-5 sm:p-6 lg:p-7 xl:p-8 2xl:p-12"
+				data-aos="zoom-in"
+				data-aos-duration="700">
 				<div className="font-bold text-gray-500 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl mb-1 sm:mb-2">
 					A short & dramatic introduction
 				</div>
@@ -25,7 +27,15 @@ export default function About() {
 					around me. Realizing my privilege and the scary lack of it in the
 					world motivated me to be purposeful about my actions and give back. I
 					got into Software Engineering professionally because it literally
-					gives one the power to bring ideas to life; here’s my CV if relevant.
+					gives one the power to bring ideas to life; here’s my{" "}
+					<a
+						href="https://docs.google.com/document/u/1/d/e/2PACX-1vSKzkGiyTeINR3S-xw28VvtHijNy_th8kbB-wnpI5qtn-do3uJsmM4EiI6l5DVPPfBsDyfBi4ZnmLHs/pub?mode=html"
+						target="_blank"
+						rel="noreferrer"
+						className={aStyle}>
+						CV
+					</a>{" "}
+					if you're interested in my professional qualifications and experience.
 					I aspire to leverage technology for sustainability, medical advances,
 					and combating inequality. Welcome to my page!
 				</div>
